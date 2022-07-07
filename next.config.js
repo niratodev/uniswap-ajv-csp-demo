0af1c506669a864f00eb7bad3d9bc9d59ad1e503
Nirato <nirato@looksrare.org>
2022-07-07 13:41:52 -0500

    csp example

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+const ContentSecurityPolicy = `
+  script-src 'self' 'unsafe-inline';
+`
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
